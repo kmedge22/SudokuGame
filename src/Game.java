d6d1181a32f402b53e90d3503dd9ae8e9a35d744
mcb4548 <mcb4548@uncw.edu>
2017-04-16 18:01:13 -0400

    Sunday 4/16/17

--- src/Game.java
+++ src/Game.java
@@ -1,65 +1,74 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
-import java.util.concurrent.ThreadLocalRandom;
-/**
- * Created by Michael Byrd on 3/31/2017.
- */
-public class Game extends JFrame{
+import java.awt.event.*;
+import java.io.*;
+import java.util.*;
 
-    public Game(){
+public class Game extends JFrame {
+
+
+    JMenuBar menuBar;
+    private JMenu fileMenu;
+    private EventHandler eh;
+    Square[][] boardArray = new Square[9][9];
+
+
+    public Game() {
         // Creating the Container
         setTitle("Sudoku");
         setSize(750, 750);
         JPanel container = new JPanel();
-        container.setLayout(new BorderLayout());;
+        container.setLayout(new BorderLayout());
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
-        JPanel panel = new JPanel();
         JPanel board = new JPanel();
-
         board.setLayout(new GridLayout(9, 9));
+        eh = new EventHandler();
 
-        Square[] squareArray = new Square[81];
-        Square[][] boardArray = new Square[9][9];
 
-        for(int i = 0; i < 9; i++){
-            for(int j = 0; j < 9; j++){
+//        Square[][] boardArray = new Square[9][9];
+        ArrayList<Character> numerals = new ArrayList<>();
+
+        for (int i = 0; i < 9; i++) {
+            numerals.add(Character.forDigit(i + 1, 10));
+        }
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
                 Square testSquare = new Square(i, j);
+                testSquare.addKeyListener(new KeyAdapter() {
+                    @Override
+                    public void keyTyped(KeyEvent keyEvent) {
+                        super.keyTyped(keyEvent);
+                        char c = keyEvent.getKeyChar();
+                        Square sq = (Square) keyEvent.getSource();
+                        for (int l = 0; l < 9; l++) {
+                            for (int k = 0; k < 9; k++) {
+                                if (sq.compareSquare(boardArray[l][k])) {
+                                    if (boardArray[l][k].getText().equals(Character.toString(c)) ||
+                                            !numerals.contains(c) ||
+                                            !sq.getText().equals("")) {
+                                        keyEvent.consume();
+                                    }
+                                }
+
+                            }
+                        }
+//                        hints(boardArray);
+                    }
+
+                    @Override
+                    public void keyReleased(KeyEvent keyEvent) {
+                        super.keyPressed(keyEvent);
+                        hints(boardArray);
+                    }
+                });
                 boardArray[i][j] = testSquare;
-//                System.out.println(testSquare);
-//                testSquare.setText(Integer.toString(i) + Integer.toString(j));
-
-//                //Coloring the Background
-//                //Box 1
-//                if(i <= 2 && 3 <= j && j <= 5){
-//                    testSquare.setBackground(Color.green);
-//                }
-//                //Box 2 and 4
-//                if((3 <= i && i <= 5) && !(3 <= j && j <= 5)){
-//                    testSquare.setBackground(Color.green);
-//                }
-//                //Box 7
-//                if(6 <= i && 3 <= j && j <= 5){
-//                    testSquare.setBackground(Color.green);
-//                }
-
-
-
+                buildMenu();
                 board.add(testSquare);
+//                pack();
             }
-
         }
 
-//        Square testSquare = new Square();
-//        board.add(testSquare);
-
+        hints(boardArray);
 
 
         container.add(board);
@@ -68,13 +77,153 @@ public class Game extends JFrame{
 
     }
 
+    private void hints(Square[][] board) {
+        ArrayList<String> hintList = new ArrayList<>();
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                Square sq = board[i][j];
+                hintList.clear();
+                for (int m = 0; m < 9; m++) {
+                    hintList.add(Integer.toString(m + 1));
+                }
+                for (int k = 0; k < 9; k++) {
+                    for (int l = 0; l < 9; l++) {
+                        if (sq.compareSquare(board[k][l])) {
 
 
-    public static void main(String[] args){
+                            if (hintList.contains(board[k][l].getText())) {
+                                hintList.remove(hintList.indexOf(board[k][l].getText()));
+                            }
+                        }
+                        sq.setToolTipText(hintList.toString());
+                    }
 
-        Game testGame = new Game();
+                }
 
-        testGame.setVisible(true);
+            }
+        }
 
     }
+
+    public static void main(String[] args) {
+        Game testGame = new Game();
+        testGame.setVisible(true);
+    }
+
+    public void buildMenu() {
+        menuBar = new JMenuBar();
+        fileMenu = new JMenu("File");
+
+        JMenuItem menuItem = new JMenuItem("New");
+        menuItem.addActionListener(eh);
+        fileMenu.add(menuItem);
+
+        menuItem = new JMenuItem("Open");
+        menuItem.addActionListener(eh);
+        fileMenu.add(menuItem);
+
+        menuItem = new JMenuItem("Save");
+        menuItem.addActionListener(eh);
+        fileMenu.add(menuItem);
+
+        menuBar.add(fileMenu);
+
+        setJMenuBar(menuBar);
+    }
+
+    private class EventHandler implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent arg0) {
+
+            if (arg0.getActionCommand().equals("Exit")) {
+                System.exit(0);
+            }
+
+            if (arg0.getActionCommand().equals("New")) {
+                for(int i = 0; i < 9; i ++){
+                    for(int j = 0; j < 9; j++){
+                        boardArray[i][j].setText("");
+                        hints(boardArray);
+//                        System.out.println(boardArray[i][j]);
+                    }
+                }
+            }
+
+            if (arg0.getActionCommand().equals("Save")) {
+                BufferedWriter writer = null;
+                JFileChooser fileChooser = new JFileChooser();
+                int returnVal = fileChooser.showSaveDialog(Game.this);
+                if (returnVal == JFileChooser.APPROVE_OPTION) {
+                    File file = fileChooser.getSelectedFile();
+                    try {
+                        writer = new BufferedWriter(new FileWriter(file));
+                        for(int i = 0; i < 9; i++){
+                            for(int j = 0; j < 9; j++){
+                                if(boardArray[i][j].getText().equals("")){
+                                    writer.write("0");
+                                }
+                                writer.write(boardArray[i][j].getText());
+                                if(j == 8){
+                                    writer.newLine();
+                                }
+                                else{
+                                    writer.write("\t");
+                                }
+                            }
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } finally {
+                        try {
+                            writer.close();
+                        } catch (Exception e) {
+                        }
+                    }
+                }
+            }
+            if (arg0.getActionCommand().equals("Open")) {
+                JFileChooser fileChooser = new JFileChooser();
+                fileChooser.showOpenDialog(null);
+                int[] lineCoord = readInts(fileChooser.getSelectedFile().getAbsolutePath());
+
+                for(int i = 0; i < lineCoord.length; i++){
+                    if(lineCoord[i] != 0) {
+                        boardArray[i / 9][i % 9].setText(Integer.toString(lineCoord[i]));
+                    }
+                }
+                hints(boardArray);
+
+            }
+        }
+
+    }
+
+    public int[] readInts(String filename) {
+        int[] data;
+        try {
+            java.util.List<Integer> values = new ArrayList<Integer>();
+            Scanner scanner = new Scanner(new File(filename));
+            while (scanner.hasNextInt()) {
+                values.add(scanner.nextInt());
+            }
+            // Copy them to an array of the exact size.
+            data = new int[values.size()];
+            Iterator<Integer> it = values.iterator();
+            int i = 0;
+            while (it.hasNext()) {
+                data[i] = it.next();
+                i++;
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Cannot find file: " + filename);
+            data = new int[0];
+        }
+        return data;
+    }
+
 }
+
+
+
+
--- src/Square.java
+++ src/Square.java
@@ -2,23 +2,15 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
 
-/**
- * Created by Michael Byrd on 3/31/2017.
- */
-public class Square extends JTextField {
-    String value;
+public class Square extends JFormattedTextField {
+    private final Character value;
+    private final ArrayList<String> availNumbers = new ArrayList<>();
 
-    ArrayList<String> availNumbers = new ArrayList<>();
-    ArrayList<String> usedNumbers = new ArrayList<>();
+    private final int rowValue;
+    private final int colValue;
+    private final int boxValue;
 
-
-
-    JTextField squareField = new JTextField(1);
-    int rowValue;
-    int colValue;
-    int boxValue;
-
-    public String getValue() {
+    public Character getValue() {
         return value;
     }
 
@@ -30,6 +22,14 @@ public class Square extends JTextField {
         return colValue;
     }
 
+    public boolean compareSquare(Square other) {
+        if (this.rowValue == other.rowValue || this.colValue == other.colValue || this.boxValue == other.boxValue) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     @Override
     public String toString() {
         return "Square{" +
@@ -38,60 +38,54 @@ public class Square extends JTextField {
                 '}';
     }
 
-    public Square(int r, int c){
+
+    public Square(int r, int c) {
         rowValue = r;
         colValue = c;
         boxValue = getBox(r, c);
+        value = '0';
 
-        if(boxValue == 1 || boxValue == 3 || boxValue == 5 || boxValue == 7){
+
+        if (boxValue == 1 || boxValue == 3 || boxValue == 5 || boxValue == 7) {
             this.setBackground(Color.green);
         }
 
-        this.setText(Integer.toString(boxValue));
 
-        String value = "";
         this.setHorizontalAlignment(JTextField.CENTER);
 
-        for(int i = 0; i < 9; i++){
-            availNumbers.add(Integer.toString(i+1));
+        for (int i = 0; i < 9; i++) {
+            availNumbers.add(Integer.toString(i + 1));
         }
 
     }
 
-    public int getBox(int r, int c){
-        if(r < 3){
-            if(c < 3){
+    private int getBox(int r, int c) {
+        if (r < 3) {
+            if (c < 3) {
                 return 0;
-            }
-            else if(2 < c && c < 6){
+            } else if (2 < c && c < 6) {
                 return 1;
-            }
-            else{
+            } else {
                 return 2;
             }
-        }
-        else if(2 < r && r < 6){
-            if(c < 3){
+        } else if (2 < r && r < 6) {
+            if (c < 3) {
                 return 3;
-            }
-            else if(2 < c && c < 6){
+            } else if (2 < c && c < 6) {
                 return 4;
-            }
-            else{
+            } else {
                 return 5;
             }
-        }
-        else{
-            if(c < 3){
+        } else {
+            if (c < 3) {
                 return 6;
-            }
-            else if(2 < c && c < 6){
+            } else if (2 < c && c < 6) {
                 return 7;
-            }
-            else{
+            } else {
                 return 8;
             }
         }
     }
+
 
 }
