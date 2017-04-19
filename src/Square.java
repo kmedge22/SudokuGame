a610cfb8a3f0527949d441bc848cc14d63684342
KEdge <kmedge22@users.noreply.github.com>
2017-04-18 22:35:43 -0400

    Update Square
    Leaving unused lists to come back and utilize later

--- src/Square.java
+++ src/Square.java
@@ -1,3 +1,6 @@
+/**
+ * Created by kelseyedge on 4/18/17.
+ */
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
@@ -9,6 +12,65 @@ public class Square extends JFormattedTextField {
     private final int rowValue;
     private final int colValue;
     private final int boxValue;
+
+    private Square[] row;
+    private Square[] col;
+    private Square[] boxes;
+
+    public Square(int r, int c) {
+        rowValue = r;
+        colValue = c;
+        boxValue = getBox(r, c);
+        value = '0';
+
+        //assign background color to visually distinguish boxes to the user
+        if (boxValue == 1 || boxValue == 3 || boxValue == 5 || boxValue == 7) {
+            this.setBackground(Color.green);
+        }
+
+        this.setHorizontalAlignment(JTextField.CENTER);
+
+        for (int i = 0; i < 9; i++) {
+            availNumbers.add(Integer.toString(i + 1));
+        }
+
+    }
+
+    public Square[] getRow() {
+        return row;
+    }
+
+    public void setRow(Square[] row) {
+        this.row = row;
+    }
+
+    public Square[] getCol() {
+        return col;
+    }
+
+    public void setCol(Square[] col) {
+        this.col = col;
+    }
+
+    public void addSquareToRow(int index, Square aSquare){
+        row[index]= aSquare;
+    }
+
+    public void addSquareToCol(int index, Square aSquare){
+        col[index]= aSquare;
+    }
+
+    public void addSquareToBox(int index, Square aSquare){
+        boxes[index]= aSquare;
+    }
+
+    public Square[] getBoxes() {
+        return boxes;
+    }
+
+    public void setBoxes(Square[] boxes) {
+        this.boxes = boxes;
+    }
 
     public Character getValue() {
         return value;
@@ -22,6 +84,11 @@ public class Square extends JFormattedTextField {
         return colValue;
     }
 
+    /**
+     * Compares two squares to identify whether they are in the corresponding row, column, or box.
+     * @param other Square to be compared
+     * @return boolean true or false
+     */
     public boolean compareSquare(Square other) {
         if (this.rowValue == other.rowValue || this.colValue == other.colValue || this.boxValue == other.boxValue) {
             return true;
@@ -30,35 +97,12 @@ public class Square extends JFormattedTextField {
         }
     }
 
-    @Override
-    public String toString() {
-        return "Square{" +
-                "rowValue=" + rowValue +
-                ", colValue=" + colValue +
-                '}';
-    }
-
-
-    public Square(int r, int c) {
-        rowValue = r;
-        colValue = c;
-        boxValue = getBox(r, c);
-        value = '0';
-
-
-        if (boxValue == 1 || boxValue == 3 || boxValue == 5 || boxValue == 7) {
-            this.setBackground(Color.green);
-        }
-
-
-        this.setHorizontalAlignment(JTextField.CENTER);
-
-        for (int i = 0; i < 9; i++) {
-            availNumbers.add(Integer.toString(i + 1));
-        }
-
-    }
-
+    /**
+     * Computes which box a Square belongs to based upon it's row and column value
+     * @param r Square's row value
+     * @param c Square's column value
+     * @return box number as int
+     */
     private int getBox(int r, int c) {
         if (r < 3) {
             if (c < 3) {
@@ -87,5 +131,14 @@ public class Square extends JFormattedTextField {
         }
     }
 
+    @Override
+    public String toString() {
+        return "Square{" +
+                "rowValue=" + rowValue +
+                ", colValue=" + colValue +
+                '}';
+    }
+
 
 }
+
