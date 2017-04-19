48e37a5f6d9250be094edc57d062cbf7a1270bbf
KEdge <kmedge22@users.noreply.github.com>
2017-04-18 22:34:35 -0400

    Separated board and game to two classes

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,6 @@
+/**
+ * Created by kelseyedge on 4/18/17.
+ */
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -5,27 +8,46 @@ import java.io.*;
 import java.util.*;
 
 public class Game extends JFrame {
-
-
+    private Board aBoard;
     JMenuBar menuBar;
     private JMenu fileMenu;
-    private EventHandler eh;
-    Square[][] boardArray = new Square[9][9];
+    private JPanel board;
 
 
     public Game() {
+        //Create Board
+        aBoard = new Board(9,9);
+
         // Creating the Container
         setTitle("Sudoku");
         setSize(750, 750);
         JPanel container = new JPanel();
         container.setLayout(new BorderLayout());
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        JPanel board = new JPanel();
+        board = new JPanel();
         board.setLayout(new GridLayout(9, 9));
-        eh = new EventHandler();
+
+        initializeBoard();
+
+        //initially show available numbers if mouse hovers
+        hints(aBoard.getBoard());
+
+        //build
+        container.add(board);
+        this.add(container);
+        buildMenu();
+        board.setVisible(true);
 
 
-//        Square[][] boardArray = new Square[9][9];
+    }
+
+    /**
+     * Makes an ArrayList of Characters. Uses ArrayList to ensure what a user enters
+     * in a square is a valid entry. Sets a Key Event to each Square so each time
+     * a value is typed in a Square, it checks validity, then talks to the hints()
+     * method to update available numbers list.
+     */
+    public void initializeBoard(){
         ArrayList<Character> numerals = new ArrayList<>();
 
         for (int i = 0; i < 9; i++) {
@@ -42,8 +64,8 @@ public class Game extends JFrame {
                         Square sq = (Square) keyEvent.getSource();
                         for (int l = 0; l < 9; l++) {
                             for (int k = 0; k < 9; k++) {
-                                if (sq.compareSquare(boardArray[l][k])) {
-                                    if (boardArray[l][k].getText().equals(Character.toString(c)) ||
+                                if (sq.compareSquare(aBoard.getSquare(l,k))) {
+                                    if (aBoard.getSquare(l,k).getText().equals(Character.toString(c)) ||
                                             !numerals.contains(c) ||
                                             !sq.getText().equals("")) {
                                         keyEvent.consume();
@@ -52,32 +74,29 @@ public class Game extends JFrame {
 
                             }
                         }
-//                        hints(boardArray);
                     }
 
                     @Override
                     public void keyReleased(KeyEvent keyEvent) {
                         super.keyPressed(keyEvent);
-                        hints(boardArray);
+                        hints(aBoard.getBoard());
                     }
                 });
-                boardArray[i][j] = testSquare;
+                aBoard.setSquare(i,j,testSquare);
                 buildMenu();
                 board.add(testSquare);
-//                pack();
             }
         }
-
-        hints(boardArray);
-
-
-        container.add(board);
-        this.add(container);
-        board.setVisible(true);
-
     }
 
-    private void hints(Square[][] board) {
+    /**
+     * Creates an ArrayList to track values that can be used in a Square. Compares this ArrayList to
+     * other squares in the same row, column, and box. If they share a common value, that value is
+     * removed from the ArrayList. A ToolTipText is set to display the ArrayList of available
+     * numbers for each square.
+     * @param board Sudoku Board
+     */
+    public void hints(Square[][] board) {
         ArrayList<String> hintList = new ArrayList<>();
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
@@ -89,41 +108,39 @@ public class Game extends JFrame {
                 for (int k = 0; k < 9; k++) {
                     for (int l = 0; l < 9; l++) {
                         if (sq.compareSquare(board[k][l])) {
-
-
                             if (hintList.contains(board[k][l].getText())) {
                                 hintList.remove(hintList.indexOf(board[k][l].getText()));
                             }
                         }
                         sq.setToolTipText(hintList.toString());
                     }
-
                 }
-
             }
         }
-
     }
 
-    public static void main(String[] args) {
-        Game testGame = new Game();
-        testGame.setVisible(true);
-    }
 
+    /**
+     * Builds MenuBar and sets action listeners to each click event of the menu items.
+     */
     public void buildMenu() {
         menuBar = new JMenuBar();
         fileMenu = new JMenu("File");
 
         JMenuItem menuItem = new JMenuItem("New");
-        menuItem.addActionListener(eh);
+        menuItem.addActionListener(e -> newMenu());
         fileMenu.add(menuItem);
 
         menuItem = new JMenuItem("Open");
-        menuItem.addActionListener(eh);
+        menuItem.addActionListener(e -> open());
         fileMenu.add(menuItem);
 
         menuItem = new JMenuItem("Save");
-        menuItem.addActionListener(eh);
+        menuItem.addActionListener(e -> save());
+        fileMenu.add(menuItem);
+
+        menuItem = new JMenuItem("Exit");
+        menuItem.addActionListener(e -> exit());
         fileMenu.add(menuItem);
 
         menuBar.add(fileMenu);
@@ -131,74 +148,86 @@ public class Game extends JFrame {
         setJMenuBar(menuBar);
     }
 
-    private class EventHandler implements ActionListener {
-
-        @Override
-        public void actionPerformed(ActionEvent arg0) {
-
-            if (arg0.getActionCommand().equals("Exit")) {
-                System.exit(0);
-            }
-
-            if (arg0.getActionCommand().equals("New")) {
-                for(int i = 0; i < 9; i ++){
-                    for(int j = 0; j < 9; j++){
-                        boardArray[i][j].setText("");
-                        hints(boardArray);
-//                        System.out.println(boardArray[i][j]);
-                    }
-                }
-            }
-
-            if (arg0.getActionCommand().equals("Save")) {
-                BufferedWriter writer = null;
-                JFileChooser fileChooser = new JFileChooser();
-                int returnVal = fileChooser.showSaveDialog(Game.this);
-                if (returnVal == JFileChooser.APPROVE_OPTION) {
-                    File file = fileChooser.getSelectedFile();
-                    try {
-                        writer = new BufferedWriter(new FileWriter(file));
-                        for(int i = 0; i < 9; i++){
-                            for(int j = 0; j < 9; j++){
-                                if(boardArray[i][j].getText().equals("")){
-                                    writer.write("0");
-                                }
-                                writer.write(boardArray[i][j].getText());
-                                if(j == 8){
-                                    writer.newLine();
-                                }
-                                else{
-                                    writer.write("\t");
-                                }
-                            }
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    } finally {
-                        try {
-                            writer.close();
-                        } catch (Exception e) {
-                        }
-                    }
-                }
-            }
-            if (arg0.getActionCommand().equals("Open")) {
-                JFileChooser fileChooser = new JFileChooser();
-                fileChooser.showOpenDialog(null);
-                int[] lineCoord = readInts(fileChooser.getSelectedFile().getAbsolutePath());
-
-                for(int i = 0; i < lineCoord.length; i++){
-                    if(lineCoord[i] != 0) {
-                        boardArray[i / 9][i % 9].setText(Integer.toString(lineCoord[i]));
-                    }
-                }
-                hints(boardArray);
-
-            }
-        }
-
+    /**
+     * Exits system
+     */
+    public void exit(){
+        System.exit(0);
     }
 
+    /**
+     * Clears the Sudoku Board
+     */
+    public void newMenu(){
+        for(int i = 0; i < 9; i ++){
+            for(int j = 0; j < 9; j++){
+                aBoard.getSquare(i,j).setText("");
+                hints(aBoard.getBoard());
+            }
+        }
+    }
+
+    /**
+     * Saves the values of Sudoku Board to a text file row by row.
+     */
+    public void save(){
+        BufferedWriter writer = null;
+        JFileChooser fileChooser = new JFileChooser();
+        int returnVal = fileChooser.showSaveDialog(Game.this);
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            File file = fileChooser.getSelectedFile();
+            try {
+                writer = new BufferedWriter(new FileWriter(file));
+                for(int i = 0; i < 9; i++){
+                    for(int j = 0; j < 9; j++){
+                        if(aBoard.getBoard()[i][j].getText().equals("")){
+                            writer.write("0");
+                        }
+                        writer.write(aBoard.getSquare(i,j).getText());
+                        if(j == 8){
+                            writer.newLine();
+                        }
+                        else{
+                            writer.write("\t");
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    writer.close();
+                } catch (Exception e) {
+                }
+            }
+        }
+    }
+
+    /**
+     * Clears any existing values on the Board. Reads in text file created
+     * from previously saved Sudoku game. Talks to readInts() method for assistance.
+     * Creates new Squares on the board matching the values of the Squares from the
+     * opened text file.
+     */
+    public void open(){
+        newMenu();
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.showOpenDialog(null);
+        int[] lineCoord = readInts(fileChooser.getSelectedFile().getAbsolutePath());
+
+        for(int i = 0; i < lineCoord.length; i++){
+            if(lineCoord[i] != 0) {
+                aBoard.getSquare(i/9,i%9).setText(Integer.toString(lineCoord[i]));
+            }
+        }
+        hints(aBoard.getBoard());
+    }
+
+    /**
+     * Helper Method to read in integers from text file.
+     * @param filename Name of file to read from
+     * @return list of integers
+     */
     public int[] readInts(String filename) {
         int[] data;
         try {
@@ -222,7 +251,15 @@ public class Game extends JFrame {
         return data;
     }
 
+
+    public static void main(String[] args) {
+        Game testGame = new Game();
+        testGame.setVisible(true);
+
+    }
+
 }
+
 
 
 
